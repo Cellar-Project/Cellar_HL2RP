26f08dc011b2edae0978626fe9aa024d0ea69fec
VintageThief <vintagethief1337@gmail.com>
2021-10-13 16:39:29 +0300

    Update npc_loot.lua

--- gamemodes/ixhl2rp/plugins/npc_loot.lua
+++ gamemodes/ixhl2rp/plugins/npc_loot.lua
@@ -6,12 +6,14 @@ local TYPE_MPF = 1
 local TYPE_ZOMBIES = 2
 local TYPE_REBELS = 3
 local TYPE_DEFAULT = 4
+local TYPE_METAL = 5
 
 local loot = {
     [TYPE_MPF] = {"bandage", "handheld_radio", "health_kit", "pistol", "zip_tie", "smg1", "pistolammo", "357ammo", "shotgunammo"},
     [TYPE_ZOMBIES] = {"water", "ration", "pistolammo", "357ammo", "shotgunammo", "suitcase", "metal_armature", "empty_can", "mat_cloth", "empty_ration", "mat_resine", "mat_wood", "mat_plastic", "electro_circuit", "box_of_nails", "box_of_needles", "box_of_casings", "box_of_gunpowder", "varnish", "chain"},
     [TYPE_REBELS] = {"chinese_takeout", "ration", "health_vial", "pistol", "smg1", "crowbar", "357ammo", "shotgunammo", "resistance_uniform", "rmedic_uniform"},
 	[TYPE_DEFAULT] = {"water", "ration", "bandana", "metal_scrap", "metal_armature", "mat_resine", "mat_cloth", "mat_wood", "mat_plastic", "empty_can", "empty_ration", "electro_circuit", "suitcase", "box_of_nails", "box_of_needles", "box_of_casings", "box_of_gunpowder", "varnish", "chain"},
+	[TYPE_METAL] = {"metal_scrap", "chain"}
 }
 
 function PLUGIN:LootGeneration(type)
@@ -27,6 +29,8 @@ end
 if SERVER then
     local npcs = {
         ["npc_combine_s"] = TYPE_MPF,
+		["npc_manhack"] = TYPE_METAL,
+		["npc_cscanner"] = TYPE_METAL,
         ["npc_combinegunship"] = TYPE_MPF,
 		["CombineElite"] = TYPE_MPF,
 		["CombinePrison"] = TYPE_MPF,
