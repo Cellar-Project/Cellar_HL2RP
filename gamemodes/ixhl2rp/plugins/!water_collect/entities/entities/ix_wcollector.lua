d9d6f4509ab918e0f4da8558ab985a0adab1e584
VintageThief <vintagethief1337@gmail.com>
2021-11-15 17:40:39 +0300

    water_collect plugin fix [WIP]

--- gamemodes/ixhl2rp/plugins/!water_collect/entities/entities/ix_wcollector.lua
+++ gamemodes/ixhl2rp/plugins/!water_collect/entities/entities/ix_wcollector.lua
@@ -12,42 +12,76 @@ ENT.bNoPersist = true
 if (SERVER) then
 	
 	--function ENT:SpawnFunction(entity, trace)
-		--local wcollectorent = ents.Create("ix_wcollector")
-		--entity:GetPos -
+	--	local wcollectorent = ents.Create("ix_wcollector")
+	--	entity:GetPos 
+	--end
 
-	--	return entity
+	local emptycont = {
+		["empty_can"] = 6,
+		["empty_glass_bottle"] = 8,
+		["empty_jug"] = 16,
+		["empty_plastic_bottle"] = 12,
+		["empty_plastic_can"] = 12,
+		["empty_tin_can"] = 6
+	}
 
 	function ENT:Initialize()
 
+		self:SetNetVar("wamount", 0)
+
 		self:SetModel("models/props_wasteland/laundry_basket001.mdl")
-		self:SetSolid(SOLD_VPHYSIC)
+		self:SetSolid(SOLID_VPHYSICS)
 		self:SetMoveType(MOVETYPE_VPHYSICS)
 		self:PhysicsInit(SOLID_VPHYSICS)
 		self:SetUseType(SIMPLE_USE)
 
 		self.nextUseTime = 0
 
+		self.timer_name = "watertimer"..self:EntIndex()
+
 		--timer
 		local conf_time = ix.config.Get("watertimer")
+		local water_n = 0
 
-		timer.Create( watertimer, conf_time, 0, function()
+		timer.Create( self.timer_name, conf_time, 0, function()
 
 			local conf_limit = ix.config.Get("waterlimit")
 			local conf_tick = ix.config.Get("watertick")
-			local water_n = 0
 		
 			if water_n <= conf_limit then
 				water_n = water_n + conf_tick
+			else
+				water_n = conf_limit
 			end
 			
-			print(water_n)
+			self:SetNetVar("wamount", water_n)
 
 		end)
 
 	end
 
 	function ENT:OnRemove()
-		timer.Remove("watertimer")
+		timer.Remove(self.timer_name)
+	end
+
+else
+
+	function ENT:Draw()
+
+		local amount = self:GetNetVar("wamount")
+
+		self:DrawModel()
+		-- text showing waterlevel over model
+		if self:GetPos():Distance(LocalPlayer():GetPos()) >= 1000 then return end
+		
+		local pos = self:GetPos()
+		local ang = self:GetAngles()
+		--ang:RotateAroundAxis(ang:Right(), -90)
+		ang.y = LocalPlayer():EyeAngles().y - 90 -- make it act like a sprite and look at the player
+
+		cam.Start3D2D(pos, ang, 1)
+			draw.SimpleTextOutlined(amount, "Default", 0, -800, Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, Color(0,0,0))
+		cam.End3D2D()	
 	end
 
 end
--- gamemodes/ixhl2rp/plugins/!water_collect/sh_plugin.lua
+++ gamemodes/ixhl2rp/plugins/!water_collect/sh_plugin.lua
@@ -5,12 +5,12 @@ PLUGIN.author = "Vintage Thief"
 PLUGIN.description = ""
 
 ix.config.Add("watertimer", 5, "How ofter water will collect (in seconds)", nil, {
-    data = {min 2, max = 3600},
+    data = {min = 2, max = 3600},
     category = "watercollector"
 })
 
 ix.config.Add("waterlimit", 5, "How much water can be in a container. (ONLY COUNTABLE)", nil, {
-    data = {min 1000, max = 10000},
+    data = {min = 1000, max = 10000},
     category = "watercollector"
 })
 
