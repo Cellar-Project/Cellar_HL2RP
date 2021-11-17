ae28d14edb075908c6f9d117715c57064ef2b945
maxxoft <ensemplixru@gmail.com>
2021-11-18 02:43:33 +0400

    few style fixes

--- gamemodes/ixhl2rp/plugins/!water_collect/entities/entities/ix_wcollector.lua
+++ gamemodes/ixhl2rp/plugins/!water_collect/entities/entities/ix_wcollector.lua
@@ -1,5 +1,13 @@
-
 local PLUGIN = PLUGIN
+
+PLUGIN.emptycont = {
+	["empty_can"] = 6,
+	["empty_glass_bottle"] = 8,
+	["empty_jug"] = 16,
+	["empty_plastic_bottle"] = 12,
+	["empty_plastic_can"] = 12,
+	["empty_tin_can"] = 6
+}
 
 ENT.Type = "anim"
 ENT.PrintName = "Water Collector"
@@ -9,21 +17,13 @@ ENT.AdminOnly = true
 ENT.PhysgunDisable = false
 ENT.bNoPersist = true
 
+
 if (SERVER) then
 	
 	--function ENT:SpawnFunction(entity, trace)
 	--	local wcollectorent = ents.Create("ix_wcollector")
 	--	entity:GetPos 
 	--end
-
-	local emptycont = {
-		["empty_can"] = 6,
-		["empty_glass_bottle"] = 8,
-		["empty_jug"] = 16,
-		["empty_plastic_bottle"] = 12,
-		["empty_plastic_can"] = 12,
-		["empty_tin_can"] = 6
-	}
 
 	function ENT:Initialize()
 
@@ -37,7 +37,7 @@ if (SERVER) then
 
 		self.nextUseTime = 0
 
-		self.timer_name = "watertimer"..self:EntIndex()
+		self.timer_name = "watertimer" .. self:EntIndex()
 
 		--timer
 		local conf_time = ix.config.Get("watertimer")
@@ -46,13 +46,13 @@ if (SERVER) then
 		local conf_tick = ix.config.Get("watertick")
 
 		timer.Create( self.timer_name, conf_time, 0, function()
-		
+
 			if water_n <= conf_limit then
 				water_n = water_n + conf_tick
 			else
 				water_n = conf_limit
 			end
-			
+
 			self:SetNetVar("wamount", water_n)
 
 		end)
@@ -69,22 +69,22 @@ else
 
 		local conf_limit = ix.config.Get("waterlimit")
 
-		local amount = (self:GetNetVar("wamount").."/"..conf_limit)
+		local amount = (self:GetNetVar("wamount") .. "/" .. conf_limit)
 
 		self:DrawModel()
 		local fixedAng = self:GetAngles()
-		fixedAng:RotateAroundAxis( self:GetUp(), 0 ) 
+		fixedAng:RotateAroundAxis( self:GetUp(), 0 )
 		fixedAng:RotateAroundAxis( self:GetRight(), 0 )
-		fixedAng:RotateAroundAxis( self:GetForward(), 0 )  
+		fixedAng:RotateAroundAxis( self:GetForward(), 0 )
 		-- text showing waterlevel over model
 		if self:GetPos():Distance(LocalPlayer():GetPos()) >= 512 then return end
-		
-		local fixedPos = self:GetPos() + self:GetUp()*10 + self:GetRight()*5 + self:GetForward()*-5
-	cam.Start3D2D(fixedPos, fixedAng, 0.1)
-			draw.RoundedBox(4,0,0,200,150, Color(0,0,0,225)) 
+
+		local fixedPos = self:GetPos() + self:GetUp() * 10 + self:GetRight() * 5 + self:GetForward() * -5
+		cam.Start3D2D(fixedPos, fixedAng, 0.1)
+			draw.RoundedBox(4, 0, 0, 200, 150, Color(0,0,0,225))
 			draw.SimpleText( "Количество воды:", "Default", 100, 0, Color( 255, 255, 255, 155 ), TEXT_ALIGN_CENTER)
 			draw.SimpleText( amount, "Default", 100, 42, Color( 255, 255, 255, 155 ), TEXT_ALIGN_CENTER)
-	cam.End3D2D()
+		cam.End3D2D()
 	end
 
 end
