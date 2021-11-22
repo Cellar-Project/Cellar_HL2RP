a5e9516f65ad1f0ff145352285ed4a23a41608d8
maxxoft <ensemplixru@gmail.com>
2021-11-22 19:02:13 +0400

    major style fixes

--- gamemodes/ixhl2rp/plugins/!farming/entities/entities/ix_seeds_potato.lua
+++ gamemodes/ixhl2rp/plugins/!farming/entities/entities/ix_seeds_potato.lua
@@ -7,58 +7,66 @@ ENT.AdminSpawnable = true
 
 if (SERVER) then
 
-    local PLUGIN = PLUGIN
+	local PLUGIN = PLUGIN
 
-    function ENT:Initialize()
-        self:SetMoveType(MOVETYPE_VPHYSICS)
+	function ENT:Initialize()
+		self:SetMoveType(MOVETYPE_VPHYSICS)
 		self:PhysicsInit(SOLID_VPHYSICS)
 		self:SetUseType(SIMPLE_USE)
 		self:SetSolid(SOLID_VPHYSICS)
 
-        local physicObject = self:GetPhysicsObject()
+		local physicsObject = self:GetPhysicsObject()
 
-        if (IsValid(physicsObject)) then
-            physicsObject:Wake()
-            physicsObject:EnableMotion(true)
-        end
-        
-        self.nextUse = 0
+		if IsValid(physicsObject) then
+			physicsObject:Wake()
+			physicsObject:EnableMotion(true)
+		end
 
-	    self.timer_name = "phasetimer" .. self:EntIndex()
+		self.nextUse = 0
 
-	    local conf_phasetime = ix.config.Get("phasetime")
-	    local growth_n = 0
-	    local phase = 0
-	    self:SetNetVar("grown", 0)
+		self.timer_name = "phasetimer" .. self:EntIndex()
 
-	    timer.Create( self.timer_name, conf_phasetime, 0, function()
-	
-	    	local conf_phaseamount = ix.config.Get("phaseamount")
-	    	local conf_phaserate = ix.config.Get("phaserate")
-	    	local conf_phases = ix.config.Get("phases")
+		local conf_phasetime = ix.config.Get("phasetime")
+		local growth_n = 0
+		local phase = 0
+		self:SetNetVar("grown", 0)
+
+		timer.Create( self.timer_name, conf_phasetime, 0, function()
+
+			local conf_phaseamount = ix.config.Get("phaseamount")
+			local conf_phaserate = ix.config.Get("phaserate")
+			local conf_phases = ix.config.Get("phases")
 			-- for в списке текстур. ибо не валид
-	    	if self:OnGround() and (SurfaceInfo:GetMaterial() == ("de_cbble/grassdirt_blend" or "nature/blenddirtdirt001a" or "highdef/metro2033/floor/floor_beton_pol_dirt3")) then
-		    	growth_n = math.Clamp( growth_n + conf_phaserate, 0, conf_phaseamount)
-		    	phase = 1
-		    	if growth_n == conf_phaseamount then
-		    		phase = math.Clamp( phase + 1, 0, conf_phases)
-		    		if phase == conf_phases then
-			    		self:SetNetVar("grown", 1)
-			    	end
-		    	end
-	    	end
-	
-	    	for k, v in ipairs(PLUGIN.growmodel) do
-	    		self:SetModel( v )
-	    	end
-      end)
-    end
+			local correctMaterial = false
+			for _, mat in pairs({"de_cbble/grassdirt_blend",
+								 "nature/blenddirtdirt001a",
+								 "highdef/metro2033/floor/floor_beton_pol_dirt3"}) do
+				if SurfaceInfo:GetMaterial() == mat then
+					correctMaterial = true
+				end
+			end
+			if self:OnGround() and correctMaterial then
+				growth_n = math.Clamp(growth_n + conf_phaserate, 0, conf_phaseamount)
+				phase = 1
+				if growth_n == conf_phaseamount then
+					phase = math.Clamp( phase + 1, 0, conf_phases)
+					if phase == conf_phases then
+						self:SetNetVar("grown", 1)
+					end
+				end
+			end
 
-    function ENT:SetSeedItem(item)
-        self:SetNetVar("item", item)
-    end
+			for k, v in ipairs(PLUGIN.growmodel) do
+				self:SetModel( v )
+			end
+	  end)
+	end
 
-	function ENT:Use( activator )
+	function ENT:SetSeedItem(item)
+		self:SetNetVar("item", item)
+	end
+
+	function ENT:Use(activator)
 
 		local item = nil 
 
@@ -67,19 +75,19 @@ if (SERVER) then
 		if isfunction(entity.GetItemTable) then
 			item = entity:GetItemTable()
 		else
-			return 
+			return
 		end
 
 		if self:GetNetVar("grown") == 1 then
-			if( activator:IsPlayer() ) then
-			
+			if activator:IsPlayer() then
+
 				local fixpos = self:GetPos() + Vector(0, 0, 30)
 				self:SetNetVar("grown", 0)
 				entity:Remove()
 
 				local amount = math.random(3, 6)
 
-				for i=0,amount do
+				for i = 0, amount do
 					ix.item.Spawn(result, fixpos)
 				end
 			end
@@ -91,16 +99,15 @@ if (SERVER) then
 	end
 
 else
-	
-	function ENT:Draw()
-        self:DrawModel()
-        
-        cam.Start2D()
-        local pos = self:GetPos():ToScreen()
-        local growth = self:GetNetVar("grown")
-        draw.DrawText(growth, "Default", pos.x, pos.y, color_white, TEXT_ALIGN_CENTER)
-        cam.End2D()
 
+	function ENT:Draw()
+		self:DrawModel()
+
+		cam.Start2D()
+		local pos = self:GetPos():ToScreen()
+		local growth = self:GetNetVar("grown")
+		draw.DrawText(growth, "Default", pos.x, pos.y, color_white, TEXT_ALIGN_CENTER)
+		cam.End2D()
 	end
 
 end
--- gamemodes/ixhl2rp/plugins/!farming/sh_plugin.lua
+++ gamemodes/ixhl2rp/plugins/!farming/sh_plugin.lua
@@ -5,24 +5,24 @@ PLUGIN.author = "Vintage Thief"
 PLUGIN.description = ""
 
 ix.config.Add("phasetime", 1, "Time a plant needs to get a point to grow to the next phase.", nil, {
-    data = {min = 1, max = 3600},
-    category = "farming"
+	data = {min = 1, max = 3600},
+	category = "farming"
 })
 
 ix.config.Add("phaserate", 1, "How much a plant gains growth points on timer tick.", nil, {
-    data = {min = 1, max = 100},
-    category = "farming"
+	data = {min = 1, max = 100},
+	category = "farming"
 })
 
 ix.config.Add("phaseamount", 10, "How much points a plant needs to get on the next phase.", nil, {
-    data = {min = 10, max = 100},
-    category = "farming"
+	data = {min = 10, max = 100},
+	category = "farming"
 })
 
 
 ix.config.Add("phases", 4, "How many phases a plant needs to fully grow.", nil, {
-    data = {min = 4, max = 4},
-    category = "farming"
+	data = {min = 4, max = 4},
+	category = "farming"
 })
 
 PLUGIN.seedplant = {
@@ -30,8 +30,8 @@ PLUGIN.seedplant = {
 }
 
 PLUGIN.growmodel = {
-    [1] = "models/props/de_train/bush.mdl",
-    [2] = "models/props_junk/cardboard_box001a.mdl",
-    [3] = "models/props_junk/cardboard_box002a.mdl",
-    [4] = "models/props/de_train/bush2.mdl",
+	[1] = "models/props/de_train/bush.mdl",
+	[2] = "models/props_junk/cardboard_box001a.mdl",
+	[3] = "models/props_junk/cardboard_box002a.mdl",
+	[4] = "models/props/de_train/bush2.mdl",
 }
