cbf2b933618467555488e7a01c586b6fd37a4fc1
maxxoft <ensemplixru@gmail.com>
2021-12-29 21:08:49 +0400

    fixed stages issues

--- gamemodes/ixhl2rp/plugins/apocalypse/sh_plugin.lua
+++ gamemodes/ixhl2rp/plugins/apocalypse/sh_plugin.lua
@@ -1,10 +1,15 @@
 local PLUGIN = PLUGIN
+
 PLUGIN.name = "Apocalypse"
 PLUGIN.author = "maxxoft"
 PLUGIN.description = "The end of times."
 
 
 if SERVER then
+	PLUGIN.models = {
+		[""] = ""
+	}
+
 	function PLUGIN:StartApocalypse()
 		local players = player.GetAll()
 
@@ -14,6 +19,7 @@ if SERVER then
 	end
 
 	function PLUGIN:InfectPlayer(client)
+		if client:IsOTA() then return end
 		local character = client:GetCharacter()
 		local hasVaccine = character:GetData("hasVaccine", false)
 
@@ -24,8 +30,12 @@ if SERVER then
 	end
 
 	function PLUGIN:AdvanceDisease(client)
+		local char = client:GetCharacter()
+		local stage = char:GetData("zstage", 0)
 
-		if client:GetCharacter():GetData("zstage") == 3 then
+		char:SetData("zstage", math.Clamp(stage + 1, 0, 3))
+
+		if char:GetData("zstage") == 3 then
 			local items = character:GetInventory():GetItemsByBase("base_weaponstest", true)
 
 			for _, item in pairs(items) do
@@ -33,6 +43,8 @@ if SERVER then
 					item:Unequip(character)
 				end
 			end
+
+			char:SetModel(self.models[char:GetModel()] or table.Random(self.models))
 		end
 	end
 end
--- gamemodes/ixhl2rp/plugins/apocalypse/sv_hooks.lua
+++ gamemodes/ixhl2rp/plugins/apocalypse/sv_hooks.lua
@@ -1,19 +1,19 @@
 local PLUGIN = PLUGIN
 
 function PLUGIN:CanPlayerEquipItem(client, item, slot)
-	if IsValid(client) and client:GetData("zombie", false) then
+	if IsValid(client) and client:GetData("zstage") == 3 then
 		return false
 	end
 end
 
 function PLUGIN:CanPlayerInteractItem(client, action)
-	if IsValid(client) and client:GetData("zombie", false) then
+	if IsValid(client) and client:GetData("zstage") == 3 then
 		return false
 	end
 end
 
 function PLUGIN:CanPlayerInteractEntity(client, entity)
-	if IsValid(client) and client:GetData("zombie", false) then
+	if IsValid(client) and client:GetData("zstage") == 3 then
 		return false
 	end
 end
@@ -22,8 +22,8 @@ function PLUGIN:EntityTraceAttack(attacker, target, trace, dmgInfo)
 	local weapon = dmgInfo:GetInflictor()
 
 	if IsValid(weapon) then
-		if weapon.IsFists and attacker:GetData("zombie", false) then
-			self:InfectPlayer(target:GetPlayer())
+		if weapon.IsFists and client:GetData("zstage") == 3 then
+			self:InfectPlayer(target:GetPlayer() or target)
 		end
 	end
 end
