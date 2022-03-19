44748831a8d785c075f973e47af83f56ed5cf7f6
maxxoft <ensemplixru@gmail.com>
2022-03-19 18:24:02 +0400

    new gasmask version

--- gamemodes/ixhl2rp/plugins/items_clothing/items/gasmask/sh_gasmask_standard.lua
+++ gamemodes/ixhl2rp/plugins/items_clothing/items/gasmask/sh_gasmask_standard.lua
@@ -1,10 +1,16 @@
 ITEM.name = "Противогаз ГП-5"
-ITEM.description = "iGasmaskDesc"
-ITEM.model = Model("models/tnb/items/gasmask.mdl")
-ITEM.rarity = 1
-ITEM.bodyGroups = {
-	[5] = 6
+ITEM.model = Model("models/cellar/items/city3/clothing/gp5.mdl")
+ITEM.width = 1 -- ширина
+ITEM.height = 1 -- высота
+ITEM.description = "Старый противогаз советского образца. Широко был распространен среди солдат НОАК, о чем свидетельствуют уже стертые иероглифы где-то внутри самого противогаза. В нем бывает довольно холодно временами."
+ITEM.slot = EQUIP_MASK -- слот ( EQUIP_MASK EQUIP_HEAD EQUIP_LEGS EQUIP_HANDS EQUIP_TORSO )
+ITEM.bodyGroups = { -- какие бодигруппы на какие сетаются
+	[1] = 5,
+	[7] = 1,
 }
+ITEM.rarity = 1
+ITEM.CanBreakDown = false -- можно ли порвать на тряпки
+ITEM.thermalIsolation = 1 -- (от 1 до 4)
 ITEM.Filters = {
 	["filter_medium"] = true,
 	["filter_standard"] = true
@@ -17,3 +23,7 @@ ITEM.Stats = {
 	[4] = 0,
 	[5] = 0,
 }
+
+function ITEM:CanEquip(client, slot)
+	return !tobool(client:GetCharacter():GetEquipment():GetItemAtSlot("head"))
+end
