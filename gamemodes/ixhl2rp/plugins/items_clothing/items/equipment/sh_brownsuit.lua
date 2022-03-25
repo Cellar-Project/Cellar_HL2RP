c4f7312f23d29d23843d5bb62a975695f25483b0
maxxoft <ensemplixru@gmail.com>
2022-03-25 20:16:30 +0400

    new clothes and uniforms [WIP]

---
+++ gamemodes/ixhl2rp/plugins/items_clothing/items/equipment/sh_brownsuit.lua
@@ -0,0 +1,11 @@
+ITEM.name = "Коричневый пиджак"
+ITEM.model = "models/cellar/items/city3/clothing/top_09.mdl"
+ITEM.width = 2 -- ширина
+ITEM.height = 2 -- высота
+ITEM.description = "Немного староватый коричневый пиджак, в комплекте с которым идет розовая рубашка. Немного мешковат, но в целом для около-деловых встреч может подойте, но для того, чтобы пережить зиму - врядли."
+ITEM.slot = EQUIP_TORSO -- слот ( EQUIP_MASK EQUIP_HEAD EQUIP_LEGS EQUIP_HANDS EQUIP_TORSO )
+ITEM.bodyGroups = { -- какие бодигруппы на какие сетаются
+    [4] = 8,
+}
+ITEM.CanBreakDown = true -- можно ли порвать на тряпки
+ITEM.thermalIsolation = 2 -- (от 1 до 4)
