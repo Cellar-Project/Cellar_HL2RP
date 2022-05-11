2c4dcc0cf73f081121969dfd6ea9378cf86b8305
maxxoft <ensemplixru@gmail.com>
2022-05-11 21:40:08 +0400

    fixed plant names

--- gamemodes/ixhl2rp/plugins/zfarming/items/farming/sh_seeds_carrot.lua
+++ gamemodes/ixhl2rp/plugins/zfarming/items/farming/sh_seeds_carrot.lua
@@ -2,4 +2,4 @@ ITEM.name = "Семена моркови"
 ITEM.model = Model("models/props_lab/box01a.mdl")
 ITEM.cost = 2
 ITEM.product = "carrot"
-ITEM.plantName = "Рассада картофеля"
+ITEM.plantName = "Рассада моркови"
--- gamemodes/ixhl2rp/plugins/zfarming/items/farming/sh_seeds_potato.lua
+++ gamemodes/ixhl2rp/plugins/zfarming/items/farming/sh_seeds_potato.lua
@@ -1,4 +1,4 @@
-ITEM.name = "Семена картофеля"
+ITEM.name = "Картофель семенной"
 ITEM.model = Model("models/props_lab/box01a.mdl")
 ITEM.cost = 2
 ITEM.product = "potato"
