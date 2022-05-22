0bc11d41203851a9d2431089ffb4b2025b681024
Andrey Karmanov <thechinapacific@gmail.com>
2022-05-22 22:22:11 +0300

    Crafting for new ammo types

--- gamemodes/ixhl2rp/plugins/ammo_cringe/items/ammostack/sh_springfield.lua
+++
@@ -1,6 +0,0 @@
-ITEM.name = "Патроны .30-06 Springfield"
-ITEM.uniqueID = "bullets_springfield"
-ITEM.model = "models/crunchy/props/stalker_props/ammo_762x54_7h1.mdl"
-ITEM.description = "Старая коробка патронов .30-06 Springfield."
-ITEM.maxStack = 16
-ITEM.defaultStack = 8
