e1791f172896824eadce936bf7bc6dfeedeb3cc8
LegAz 2.0 <100089643+offlegaz2@users.noreply.github.com>
2022-05-23 21:26:48 +0300

    Draw motion blur instead of helix blur in limb_penalties

--- gamemodes/ixhl2rp/plugins/limbs_penalties/cl_hooks.lua
+++ gamemodes/ixhl2rp/plugins/limbs_penalties/cl_hooks.lua
@@ -1,10 +1,14 @@
 
 -- blur clients screen if head is hurt
 function PLUGIN:RenderScreenspaceEffects()
-	local hDamageFraction = self:GetLimbsDamage(LocalPlayer(), true, "head")[1]
+	local client = LocalPlayer()
 
-	if (isnumber(hDamageFraction)) then
-		ix.util.DrawBlurAt(0, 0, ScrW(), ScrH(), nil, nil, hDamageFraction * 255)
+	if (client:GetMoveType() != MOVETYPE_NOCLIP) then
+		local hDamageFraction = self:GetLimbsDamage(client, true, "head")[1]
+
+		if (isnumber(hDamageFraction)) then
+			DrawMotionBlur(0.1, 1 * hDamageFraction, 0.01)
+		end
 	end
 end
 
