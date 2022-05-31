f3c51f9731e4c2e0949f075408de315cb33c6acb
LegAz <100089643+offlegaz2@users.noreply.github.com>
2022-05-31 22:42:53 +0300

    Set food conservation data manually instead of SetData function

--- gamemodes/ixhl2rp/plugins/food/cl_plugin.lua
+++ gamemodes/ixhl2rp/plugins/food/cl_plugin.lua
@@ -4,6 +4,7 @@ net.Receive("ixFoodConservation", function()
 	local dateToSet = net.ReadUInt(32)
 	local timeLeftToSet = net.ReadUInt(32)
 
-	item:SetData("expirationDate", dateToSet)
-	item:SetData("expirationTimeLeft", timeLeftToSet != 0 and timeLeftToSet or nil)
+	item.data = item.data or {}
+	item.data["expirationDate"] = dateToSet
+	item.data["expirationTimeLeft"] = timeLeftToSet != 0 and timeLeftToSet or nil
 end)
