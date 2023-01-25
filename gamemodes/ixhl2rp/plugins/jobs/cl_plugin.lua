7030c43d4208beb36317bc1789ad26ba6279ce71
maxxoft <ensemplixru@gmail.com>
2023-01-25 22:41:38 +0400

    spaces to tabs; entity for new job

--- gamemodes/ixhl2rp/plugins/jobs/cl_plugin.lua
+++ gamemodes/ixhl2rp/plugins/jobs/cl_plugin.lua
@@ -2,26 +2,26 @@ local PLUGIN = PLUGIN
 
 
 surface.CreateFont("ixTaskMain", {
-    font = "Roboto Lt",
-    size = 22,
-    weight = 300,
-    antialias = true,
-    extended = true
+	font = "Roboto Lt",
+	size = 22,
+	weight = 300,
+	antialias = true,
+	extended = true
 })
 
 surface.CreateFont("ixTaskName", {
-    font = "Roboto",
-    size = 18,
-    weight = 300,
-    antialias = true,
-    extended = true
+	font = "Roboto",
+	size = 18,
+	weight = 300,
+	antialias = true,
+	extended = true
 })
 surface.CreateFont("ixTaskDesc", {
-    font = "Roboto Cn",
-    size = 16,
-    weight = 300,
-    antialias = true,
-    extended = true
+	font = "Roboto Cn",
+	size = 16,
+	weight = 300,
+	antialias = true,
+	extended = true
 })
 
 local PANEL = {}
@@ -29,168 +29,168 @@ PANEL.minW = 300
 PANEL.minH = 100
 
 function PANEL:Init()
-    if IsValid(ix.gui.quest) then
-        ix.gui.quest:Remove()
-    end
+	if IsValid(ix.gui.quest) then
+		ix.gui.quest:Remove()
+	end
 
-    ix.gui.quest = self
+	ix.gui.quest = self
 
-    timer.Create("ixQuestsFetch", 0.5, 0, function()
-        local character = LocalPlayer():GetCharacter()
+	timer.Create("ixQuestsFetch", 0.5, 0, function()
+		local character = LocalPlayer():GetCharacter()
 
-        if character then
-            local quests = character:GetData("quests", {})
-            if table.Count(quests) > 0 and !ix.gui.quest:IsVisible() then
-                ix.gui.quest:SetVisible(true)
-            elseif table.Count(quests) <= 0 then
-                if ix.gui.quest:IsVisible() then
-                    ix.gui.quest:SetVisible(false)
-                end
-            end
-        else
-            ix.gui.quest:SetVisible(false)
-        end
-    end)
+		if character then
+			local quests = character:GetData("quests", {})
+			if table.Count(quests) > 0 and !ix.gui.quest:IsVisible() then
+				ix.gui.quest:SetVisible(true)
+			elseif table.Count(quests) <= 0 then
+				if ix.gui.quest:IsVisible() then
+					ix.gui.quest:SetVisible(false)
+				end
+			end
+		else
+			ix.gui.quest:SetVisible(false)
+		end
+	end)
 
-    self:SetVisible(false)
+	self:SetVisible(false)
 
-    self.quests = {}
-    self:SetSize(self.minW, self.minH)
-    self:SetPos(ScrW() - self.minW, 10)
-    self.title = self:Add("DLabel")
-    self.title:SetFont("ixTaskMain")
-    self.title:SetText("ЗАДАНИЯ")
-    self.title:SizeToContents()
-    self.title:Dock(TOP)
+	self.quests = {}
+	self:SetSize(self.minW, self.minH)
+	self:SetPos(ScrW() - self.minW, 10)
+	self.title = self:Add("DLabel")
+	self.title:SetFont("ixTaskMain")
+	self.title:SetText("ЗАДАНИЯ")
+	self.title:SizeToContents()
+	self.title:Dock(TOP)
 
-    self.container = self:Add("Panel")
-    self.container:Dock(TOP)
+	self.container = self:Add("Panel")
+	self.container:Dock(TOP)
 
-    self:InvalidateLayout(true)
-    self:SizeToChildren(false, true)
+	self:InvalidateLayout(true)
+	self:SizeToChildren(false, true)
 end
 
 function PANEL:RemoveQuest(id)
-    if self.quests[id] then
-        self.quests[id]:Remove()
-    end
+	if self.quests[id] then
+		self.quests[id]:Remove()
+	end
 
-    self.container:InvalidateLayout(true)
-    self.container:SizeToChildren(false, true)
+	self.container:InvalidateLayout(true)
+	self.container:SizeToChildren(false, true)
 
-    self:InvalidateLayout(true)
-    self:SizeToChildren(false, true)
+	self:InvalidateLayout(true)
+	self:SizeToChildren(false, true)
 end
 
 function PANEL:AddQuest(id, text, value)
-    local b = self.container:Add("Panel")
-    b:Dock(TOP)
-    b:DockMargin(0, 5, 0, 0)
-    b:DockPadding(5, 5, 5, 5)
-    b.Paint = function(s, w, h)
-        surface.SetDrawColor(25, 25, 25, 225)
-        surface.DrawRect(0, 0, w, h)
+	local b = self.container:Add("Panel")
+	b:Dock(TOP)
+	b:DockMargin(0, 5, 0, 0)
+	b:DockPadding(5, 5, 5, 5)
+	b.Paint = function(s, w, h)
+		surface.SetDrawColor(25, 25, 25, 225)
+		surface.DrawRect(0, 0, w, h)
 
-        surface.SetDrawColor(90, 90, 90, 255)
-        surface.DrawOutlinedRect(0, 0, w, h)
-    end
+		surface.SetDrawColor(90, 90, 90, 255)
+		surface.DrawOutlinedRect(0, 0, w, h)
+	end
 
-    local title = b:Add("DLabel")
-    title:SetFont("ixTaskName")
-    title:SetText(text)
-    title:SizeToContents()
-    title:Dock(TOP)
+	local title = b:Add("DLabel")
+	title:SetFont("ixTaskName")
+	title:SetText(text)
+	title:SizeToContents()
+	title:Dock(TOP)
 
-    title = b:Add("DLabel")
-    title:SetFont("ixTaskDesc")
-    title:DockMargin(0, 2, 0, 0)
-    title:SetText("")
-    title:SizeToContents()
-    title:Dock(TOP)
+	title = b:Add("DLabel")
+	title:SetFont("ixTaskDesc")
+	title:DockMargin(0, 2, 0, 0)
+	title:SetText("")
+	title:SizeToContents()
+	title:Dock(TOP)
 
-    local timerID = "ixQuestText" .. id
-    timer.Create(timerID, 0.25, 0, function()
-        if !IsValid(ix.gui.quest) or !IsValid(title) then
-            timer.Remove(timerID)
-        end
+	local timerID = "ixQuestText" .. id
+	timer.Create(timerID, 0.25, 0, function()
+		if !IsValid(ix.gui.quest) or !IsValid(title) then
+			timer.Remove(timerID)
+		end
 
-        if IsValid(title) then
-            title:SetText(value())
-        end
-    end)
+		if IsValid(title) then
+			title:SetText(value())
+		end
+	end)
 
-    b:InvalidateLayout(true)
-    b:SizeToChildren(false, true)
+	b:InvalidateLayout(true)
+	b:SizeToChildren(false, true)
 
-    self.quests[id] = b
+	self.quests[id] = b
 
-    self.container:InvalidateLayout(true)
-    self.container:SizeToChildren(false, true)
+	self.container:InvalidateLayout(true)
+	self.container:SizeToChildren(false, true)
 
-    self:InvalidateLayout(true)
-    self:SizeToChildren(false, true)
+	self:InvalidateLayout(true)
+	self:SizeToChildren(false, true)
 end
 
 vgui.Register("ixQuest", PANEL, "EditablePanel")
 
 QuestData = QuestData or {}
 QuestData["cwu_garbage"] = {
-    title = "Ежедневная работа в ГСР",
-    func = function()
-        return string.format("Собрать в городе мусор [%s/4]", LocalPlayer():GetCharacter():GetData("cwuGarbage", 0))
-    end
+	title = "Ежедневная работа в ГСР",
+	func = function()
+		return string.format("Собрать в городе мусор [%s/4]", LocalPlayer():GetCharacter():GetData("cwuGarbage", 0))
+	end
 }
 QuestData["cwu_water"] = {
-    title = "Ежедневная работа в ГСР",
-    func = function()
-        return string.format("Заменить картриджи в автоматах [%s/3]", LocalPlayer():GetCharacter():GetData("cwuWater", 0))
-    end
+	title = "Ежедневная работа в ГСР",
+	func = function()
+		return string.format("Заменить картриджи в автоматах [%s/3]", LocalPlayer():GetCharacter():GetData("cwuWater", 0))
+	end
 }
 
 net.Receive("ixUpdateQuests", function(len)
-    local character = LocalPlayer():GetCharacter()
-    local quests = character:GetData("quests", {})
+	local character = LocalPlayer():GetCharacter()
+	local quests = character:GetData("quests", {})
 
-    if !IsValid(ix.gui.quest) then vgui.Create("ixQuest") end
+	if !IsValid(ix.gui.quest) then vgui.Create("ixQuest") end
 
-    ix.gui.quest.container:Clear()
-    ix.gui.quest.container:InvalidateLayout(true)
-    ix.gui.quest.container:SizeToChildren(false, true)
-    ix.gui.quest:InvalidateLayout(true)
-    ix.gui.quest:SizeToChildren(false, true)
+	ix.gui.quest.container:Clear()
+	ix.gui.quest.container:InvalidateLayout(true)
+	ix.gui.quest.container:SizeToChildren(false, true)
+	ix.gui.quest:InvalidateLayout(true)
+	ix.gui.quest:SizeToChildren(false, true)
 
-    if table.Count(quests) > 0 then
-        for k, v in pairs(quests) do
-            local data = QuestData[k]
-            if data then
-                ix.gui.quest:AddQuest(k, data.title, data.func)
-            end
-        end
-    end
+	if table.Count(quests) > 0 then
+		for k, v in pairs(quests) do
+			local data = QuestData[k]
+			if data then
+				ix.gui.quest:AddQuest(k, data.title, data.func)
+			end
+		end
+	end
 end)
 
 function PLUGIN:CharacterLoaded(character)
-    vgui.Create("ixQuest")
+	vgui.Create("ixQuest")
 end
 
 if IsValid(ix.gui.quest) then
-    vgui.Create("ixQuest")
+	vgui.Create("ixQuest")
 
-    local character = LocalPlayer():GetCharacter()
-    local quests = character:GetData("quests", {})
+	local character = LocalPlayer():GetCharacter()
+	local quests = character:GetData("quests", {})
 
-    ix.gui.quest.container:Clear()
-    ix.gui.quest.container:InvalidateLayout(true)
-    ix.gui.quest.container:SizeToChildren(false, true)
-    ix.gui.quest:InvalidateLayout(true)
-    ix.gui.quest:SizeToChildren(false, true)
+	ix.gui.quest.container:Clear()
+	ix.gui.quest.container:InvalidateLayout(true)
+	ix.gui.quest.container:SizeToChildren(false, true)
+	ix.gui.quest:InvalidateLayout(true)
+	ix.gui.quest:SizeToChildren(false, true)
 
-    if table.Count(quests) > 0 then
-        for k, v in pairs(quests) do
-            local data = QuestData[k]
-            if data then
-                ix.gui.quest:AddQuest(k, data.title, data.func)
-            end
-        end
-    end
+	if table.Count(quests) > 0 then
+		for k, v in pairs(quests) do
+			local data = QuestData[k]
+			if data then
+				ix.gui.quest:AddQuest(k, data.title, data.func)
+			end
+		end
+	end
 end
--- gamemodes/ixhl2rp/plugins/jobs/sv_hooks.lua
+++ gamemodes/ixhl2rp/plugins/jobs/sv_hooks.lua
@@ -2,23 +2,24 @@ local PLUGIN = PLUGIN
 
 
 util.AddNetworkString("ixUpdateQuests")
+util.AddNetworkString("ixQuestPickup")
 
 function PLUGIN:OnPlayerClearGarbage(client)
-    local character = client:GetCharacter()
-    local quests = character:GetData("quests", {})
+	local character = client:GetCharacter()
+	local quests = character:GetData("quests", {})
 
-    if quests["cwu_garbage"] then
-        local garbages = character:GetData("cwuGarbage", 0)
+	if quests["cwu_garbage"] then
+		local garbages = character:GetData("cwuGarbage", 0)
 
-        if garbages < 4 then
-            character:SetData("cwuGarbage", math.Clamp(garbages + 1, 0, 4))
-        end
-    end
+		if garbages < 4 then
+			character:SetData("cwuGarbage", math.Clamp(garbages + 1, 0, 4))
+		end
+	end
 end
 
 function PLUGIN:CharacterLoaded(character)
-    timer.Simple(1, function()
-        net.Start("ixUpdateQuests")
-        net.Send(character:GetPlayer())
-    end)
+	timer.Simple(1, function()
+		net.Start("ixUpdateQuests")
+		net.Send(character:GetPlayer())
+	end)
 end
